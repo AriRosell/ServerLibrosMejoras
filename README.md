745805460631eb1590337a3bed073d3030d85500
AriRosell <arigallegos1999@gmail.com>
2023-08-21 04:55:12 +0000

    Mejoras a repositorio ServerLibros

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ServerLibros
-descarga libros desde el servidor
+Este repositorio contiene scripts de python (se requiere instalación) que ayudan en la descarga de los nuevos (2023) libros de la SEP para primaria
+
+Mejoras:
+El README no contenía información relevante sobre qué herramientas se van a necesitar para la ejecución del programa.
+
+Se realizaron mejoras en el archivo primergrado.py, donde ya se puede ejecutar el script (marcaba un error por falta de permisos). Además, se hizo que ya no se descargaran imagenes que no existen en los libros originales.
+
+El proceso se realiza de forma más lenta, pero con la certeza de que recibiremos unicamente la informacion que nos importa.
--- Scripts/primergrado.py
+++ Scripts/primergrado.py
@@ -1,41 +1,57 @@
 import os
 import requests
 
-urlBase = "https://www.conaliteg.sep.gob.mx"
+urlBase = "https://www.conaliteg.sep.gob.mx/2023/c/"
+
+#Al usar esta url un tanto diferente podemos acceder directmente a donde se encuentre guardada la imágen, por lo tanto si no existe, no bajará ningún archivo 
+
 links = [
-    "/2023/P1MLA.htm",
-    "/2023/P1PAA.htm",
-    "/2023/P1PCA.htm",
-    "/2023/P1TPA.htm",
-    "/2023/P1SDA.htm",
-    "/2023/P1PEA.htm",
+    "P1MLA",
+    "P1PAA",
+    "P1PCA",
+    "P1TPA",
+    "P1SDA",
+    "P1PEA",
     # Agrega más enlaces aquí
 ]
 
+
+
 # Rango de imágenes a descargar (por ejemplo, del 1 al 100)
-inicioIndex = 1
-finalIndex = 300
+#inicioIndex = 1
+#finalIndex = 300
+
+#Ya no es necesario usar un rango de imagenes
 
 # Carpeta de destino para guardar las imágenes descargadas
-folderDestino = "/home/ubuntu/librosPrimerGrado"
-os.makedirs(folderDestino, exist_ok=True)
+folderDestino = os.path.join(os.getcwd(), "librosPrimerGrado")
+os.mkdir(folderDestino)
+
+#De esta manera podemos crear la carpeta sin necesidad de tener permisos extras
 
 for link in links:
+
+    count = 0
+    seguir = True
+
     full_url = urlBase + link
 
-    for imagenIndex in range(inicioIndex, finalIndex + 1):
-        imagenUrl = f"{full_url}#{imagenIndex:03d}.jpg"
+    while seguir:
+        imagenUrl = f"{full_url}/{count:03d}.jpg"
 
         response = requests.get(imagenUrl)
         if response.status_code == 200:
-            imagenNombreSecuencia = f"{link.split('/')[-1]}_{imagenIndex:03d}.jpg"
+            imagenNombreSecuencia = f"{link}_{count:03d}.jpg"
             imagenPath = os.path.join(folderDestino, imagenNombreSecuencia)
 
             with open(imagenPath, "wb") as img_file:
                 img_file.write(response.content)
 
             print(f"Imagen {imagenNombreSecuencia} descargada")
+            count += 1
+
         else:
             print(f"Error al descargar la imagen {imagenUrl}")
+            seguir = False
 
 print("Proceso de descarga de imágenes completado.")
